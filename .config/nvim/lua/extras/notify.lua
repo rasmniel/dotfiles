7a8ec88d464a6e3cdca4806a2626a73bffcf3e2e
Rasmus Nielsen <rasmniel@gmail.com>
2026-06-24 17:37:08 +0200

    Use fidget.notify for vim.notify

--- .config/nvim/lua/extras/notify.lua
+++ .config/nvim/lua/extras/notify.lua
@@ -1,6 +1,8 @@
 local _, fidget = pcall(require, 'fidget')
 local notify = fidget.notify or vim.notify
 
+vim.notify = fidget.notify
+
 return {
     notify = notify,
     -- This version of notify is required for some handlers that do not allow Neovim to run notify inside "fast" handler scopes.
