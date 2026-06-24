1d95a0990c2c3cc825ad54608129c97d129f105b
Rasmus Nielsen <rasmniel@gmail.com>
2026-06-24 17:30:21 +0200

    Neovim architecture check

--- .files/setup/packages/nvim.sh
+++ .files/setup/packages/nvim.sh
@@ -1,5 +1,7 @@
 setup_nvim() {
-    curl -LO https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.tar.gz
+    arch="x86_64"
+    test "$(uname -m)" = aarch64 && arch="arm64"
+    curl -LO "https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-$arch.tar.gz"
     mkdir -p ~/.nvim
     tar -xzf nvim-linux-x86_64.tar.gz --strip-components=1 -C ~/.nvim
     rm nvim-linux-x86_64.tar.gz
